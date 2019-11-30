c637a42729ee484e56980c0675bb847715531008
Tepp <Madis.Tepp@sas.se>
2019-11-30 09:17:55 +0100

    maps function fixed, commented out to save api

---
+++ assets/js/maps.js
@@ -0,0 +1,6 @@
+//commented out to avoid usage of maps APi//function initMap() { 
+//  var map = new google.maps.Map(document.getElementById('map'), {
+///    center: { lat: 59.4370, lng: 24.7536 },
+/// zoom: 10
+/// });
+//};
