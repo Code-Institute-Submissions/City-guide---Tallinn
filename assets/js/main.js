a5ca8e5af4a13299fc38a235df6c0f48bad62902
Tepp <Madis.Tepp@sas.se>
2019-12-03 19:58:00 +0100

    main.js init commit. moveTo moveToTravel created

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+//click on top nav button to move further down the page to specific section//
+function moveTo() {
+    let findOnPage = document.getElementById("recommended-event");
+    findOnPage.scrollIntoView();
+
+}
+
+function moveToTravel() {
+    let findOnPage = document.getElementById("travel");
+    findOnPage.scrollIntoView();
+
+}
