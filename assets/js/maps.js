76549aefeaeb2bbc96e3f47538c43851d7d3d29a
Tepp <Madis.Tepp@sas.se>
2019-12-07 19:55:55 +0100

    function to get input value

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -8,34 +8,13 @@ function initMap() {
     });
 
 
-
-
-
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
-    });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 };
-var locations = [
-    { lat: 59.451395, lng: 24.7340436 },
-    { lat: 59.4514966, lng: 24.7275039 },
-    { lat: 59.438427, lng: 24.726175 },
-    { lat: 59.439829, lng: 24.7267392 },
-    { lat: 59.436212, lng: 24.7938084 },
-    { lat: 59.4369247, lng: 24.7403846 },
-    { lat: 59.4354554, lng: 24.7431941 },
-    { lat: 59.4520567, lng: 24.7261848 },
-    { lat: 59.4382285, lng: 24.7417204 },
-    { lat: 59.4374215, lng: 24.7504393 },
-    { lat: 59.4385847, lng: 24.7259518 },
-    { lat: 59.4389342, lng: 24.7239269 },
-    { lat: 59.435945, lng: 24.7455922 },
-    { lat: 59.4392169, lng: 24.7258886 },
-    { lat: 59.432702, lng: 24.7426103 }
-];
+
+
+///search function return string ///
+
+
+function searchTxt() {
+    var inputTxt = document.getElementById('map_search').value;
+    console.log(inputTxt);
+};
