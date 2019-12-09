508f28832ca349a6336fbf14113f61e32a9fe9ae
Tepp <Madis.Tepp@sas.se>
2019-12-09 18:43:10 +0100

    maps script reset to only show maps due error

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,19 +1,11 @@
 //parts of below code is taken from developer.google.com as it's a part of google.maps API documentation
 //and is required to make google maps function
-
 function initMap() {
+
+    infowindow = new google.maps.InfoWindow();
+
     let map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 59.4370, lng: 24.7536 },
         zoom: 10
     });
-
-    var location = getElementById('map_search');
-
-    var autocomplete = new google.maps.places.Autocomplete(location);
-
-    autocomplete.setFields(
-        ['address_components', 'geometry', 'icon', 'name']);
-
-
-    autocomplete.bindTo('bounds', map);
 };
