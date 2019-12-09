dc46f90215d7a1b88e6399459fe040c1dd51cbc3
Tepp <Madis.Tepp@sas.se>
2019-12-09 20:22:39 +0100

    map reset due unsuccesful places impl.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,6 @@
 //parts of below code is taken from developer.google.com as it's a part of google.maps API documentation
 //and is required to make google maps function
 function initMap() {
-
-    infowindow = new google.maps.InfoWindow();
-
     let map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 59.4370, lng: 24.7536 },
         zoom: 10
