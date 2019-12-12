726464700224be0d4acf20f7699d7b5880e6a5ea
Tepp <Madis.Tepp@sas.se>
2019-12-12 20:52:53 +0100

    comment updated

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,13 @@
-//parts of below code is taken from developer.google.com as it's a part of google.maps API documentation
-//and is required to make google maps function
+//most parts of below code is taken from developer.google.com from google.maps API documentation
+
+
 function initMap() {
+
+
     let map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 59.4370, lng: 24.7536 },
         zoom: 10
     });
 
-    var searchInput = document.getElementById('map_search').value;
+
 };
