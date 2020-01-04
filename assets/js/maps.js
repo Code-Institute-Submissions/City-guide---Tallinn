651965b8675b6b9757e2829a7cae7f78e57425ec
Tepp <Madis.Tepp@sas.se>
2020-01-04 21:10:32 +0100

    getNearByPlaces keyword updated

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,5 +1,8 @@
+/////////code copied from developers.google.com and adjusted to work for current webpage//////
+
 let pos;
 let map;
+
 
 function initMap() {
 
@@ -12,35 +15,39 @@ function initMap() {
     getNearbyPlaces(pos);
 }
 
+
 function getNearbyPlaces(position) {
 
     let request = {
         location: position,
         rankBy: google.maps.places.RankBy.DISTANCE,
-        keyword: 'seaplane hangar'
+        keyword: 'linnahall'
     };
 
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, nearbyCallback);
 }
 
+
+
 function nearbyCallback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
         createMarkers(results);
     }
+
 }
+
+
 
 function createMarkers(places) {
     places.forEach(place => {
         let marker = new google.maps.Marker({
             position: place.geometry.location,
             map: map,
-            title: place.name
+            title: place.name,
         });
 
 
-        bounds.extend(place.geometry.location);
     });
 
-    map.fitBounds(bounds);
 }
