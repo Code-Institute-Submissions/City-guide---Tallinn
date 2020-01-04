1baf3617569befa0f4a52fce1bc7995cceb00b45
Tepp <Madis.Tepp@sas.se>
2020-01-04 15:59:09 +0100

    maps function redone to show nearby places

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,87 +1,46 @@
-//most parts of below code is taken from developer.google.com from google.maps API documentation
-
+let pos;
+let map;
 
 function initMap() {
 
-
-    let map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 59.4370, lng: 24.7536 },
-        zoom: 10
+    pos = { lat: 59.4370, lng: 24.7536 };
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pos,
+        zoom: 12
     });
 
-    var searchInput = document.getElementById('map_search');
+    getNearbyPlaces(pos);
+}
 
-    //autocomplete and search function code has been taken from codexworld solution
-    //with some adjustments made to personalise for this site
+function getNearbyPlaces(position) {
 
-    var autocomplete = new google.maps.places.Autocomplete(searchInput);
-    autocomplete.bindTo('bounds', map)
+    let request = {
+        location: position,
+        rankBy: google.maps.places.RankBy.DISTANCE,
+        keyword: 'seaplane hangar'
+    };
 
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(59.4370, 24.7536)
-    })
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, nearbyCallback);
+}
 
+function nearbyCallback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        createMarkers(results);
+    }
+}
 
-
-
-    autocomplete.addListener('place_changed', function() {
-        infowindow.close();
-        marker.setVisible(false);
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            window.alert("No geometry provided for location!")
-            return;
-        }
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(16);
-        }
-        marker.setIcon(({
-            url: place.icon,
-            scaledSize: new google.maps.Size(35, 35)
-        }));
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-
-
-
-
-
-    })
-
-
-
-    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
+function createMarkers(places) {
+    places.forEach(place => {
+        let marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name
         });
+
+
+        bounds.extend(place.geometry.location);
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
-};
-
-var locations = [
-    { lat: 59.4516884, lng: 24.7383098 },
-    { lat: 59.4515258, lng: 24.7296698 },
-    { lat: 59.4383745, lng: 24.7283514 },
-    { lat: 59.439518, lng: 24.7284773 },
-    { lat: 59.4361949, lng: 24.7961812 },
-    { lat: 59.4369154, lng: 24.7424514 },
-    { lat: 59.4354577, lng: 24.7452601 },
-    { lat: 59.4520651, lng: 24.7282848 },
-    { lat: 59.4382365, lng: 24.7438458 },
-    { lat: 59.437424, lng: 24.7526194 },
-    { lat: 59.4359316, lng: 24.7477253 },
-    { lat: 59.438617, lng: 24.7280598 },
-    { lat: 59.4389384, lng: 24.7260338 },
-    { lat: 59.4392068, lng: 24.7279627 },
-    { lat: 59.4327102, lng: 24.7447364 },
-
-]
+    map.fitBounds(bounds);
+}
