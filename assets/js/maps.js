67309bcbbd670f12e7e97bb8d2bc48dfcfd10227
Tepp <Madis.Tepp@sas.se>
2020-01-05 20:48:14 +0100

    location bias searchBox function fixed

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,6 +4,11 @@ let pos;
 let map;
 
 
+
+
+/// let searchInfo; - commented out as part of nearbyplaces function which is not used at this stage
+
+
 function initMap() {
 
     pos = { lat: 59.4370, lng: 24.7536 };
@@ -12,17 +17,98 @@ function initMap() {
         zoom: 12
     });
 
-    getNearbyPlaces(pos);
+    var defaultBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(59.4407433, 24.5408145),
+        new google.maps.LatLng(59.397255, 25.0244483));
+
+    var input = document.getElementById('map_search');
+
+    var options = {
+
+        types: ['establishment'],
+        componentRestrictions: { country: 'ee' }
+    };
+
+    var searchBox = new google.maps.places.SearchBox(input, options, {
+        bounds: defaultBounds
+    });
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+
+
+    searchBox.addListener('places_changed', function() {
+
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+
+        });
+        map.fitBounds(bounds);
+
+    });
+
+
+
+
+    // getNearbyPlaces(pos); -- commented out as nearbyplaces function not used at this stage 
 }
 
+
+
+
+/* code commented out as not relevant at this point
 
 function getNearbyPlaces(position) {
 
     let request = {
         location: position,
         rankBy: google.maps.places.RankBy.DISTANCE,
-        keyword: 'linnahall'
+        keyword: 'kivi paber käärid'
+
     };
+
 
     service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, nearbyCallback);
@@ -38,6 +124,10 @@ function nearbyCallback(results, status) {
 }
 
 
+function findPlaceFromQuery(query, position) {
+
+}
+
 
 function createMarkers(places) {
     places.forEach(place => {
@@ -50,4 +140,5 @@ function createMarkers(places) {
 
     });
 
-}
+
+} */
